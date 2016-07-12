b040b79fe7e29247f4dedb1b2ee848060baefaf6
DeepEye <DeepEye@Gmail.com>
2016-07-12 15:59:38 +0900

    1. 필드에 스크립트 추가함
    2. Initialize 스크립트 추가함

---
+++ Program/UootNori/Assets/Scripts/Initialize.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UootNori;
+
+public class Initialize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameData.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
