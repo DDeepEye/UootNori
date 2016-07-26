c4a7fef42339a613b0e9b43f1fbe1a948e079bdb
unknown <>
2016-07-27 05:25:51 +0900

    윷 던지는 애니

--- Program/UootNori/Assets/Scripts/Rule/UootThrow.cs
+++ Program/UootNori/Assets/Scripts/Rule/UootThrow.cs
@@ -13,9 +13,10 @@ public class UootThrow : Attribute {
     걸: (0.6^3)*(0.4)*(4C1)=0.3456(약 35%)
     윷: (0.6^4)=0.1296(약 13%)
     모: (0.4^4)=0.0256(약 3%)
-        낙: (0.6)*(0.4^3)*(4C1)=0.0512(약 5.12%) 
-        빽도: (0.6)*(0.4^3)*(4C1)=0.0512(약 5.12%) 
-        */
+    낙: (0.6)*(0.4^3)*(4C1)=0.0512(약 5.12%) 
+    빽도: (0.6)*(0.4^3)*(4C1)=0.0512(약 5.12%) 
+    */
+
     public const int DO = 1536;
     public const int GE = 3456;
     public const int GUL = 3456;
@@ -29,9 +30,139 @@ public class UootThrow : Attribute {
 
     List<int> _animalProbability = new List<int>();
     int [] _probabilityOffset = new int[(int)Animal.MAX];
+    Animator _uootAni;
+    GameObject _uootAniObj;
+    int _curAniIndex;
 
-	// Use this for initialization
-	void Start () {
+    const int UOOT_NUM = 4;
+    GameObject [] _uoots = new GameObject[UOOT_NUM];
+    
+    delegate void UootAnimaion();
+    UootAnimaion[] _uootAnimaion = new UootAnimaion[(int)UootNori.Animal.MAX];
+
+    void UootAniInit()
+    {
+        if(_uootAniObj == null)
+        {
+            _uootAniObj = GameObject.Find("Uoot_ani");
+            
+            _uootAni = _uootAniObj.GetComponent<Animator>();
+            Object normal = Resources.Load("Uoot/Uoot_N");
+            Object back = Resources.Load("Uoot/Uoot_B");
+
+            _uoots[0] = GameObject.Instantiate(normal, new Vector3(0.0f, 0.0f, 0.0f),new Quaternion()) as GameObject;
+            _uoots[1] = GameObject.Instantiate(normal, new Vector3(0.0f, 0.0f, 0.0f), new Quaternion()) as GameObject;
+            _uoots[2] = GameObject.Instantiate(normal, new Vector3(0.0f, 0.0f, 0.0f), new Quaternion()) as GameObject;
+            _uoots[3] = GameObject.Instantiate(back, new Vector3(0.0f, 0.0f, 0.0f), new Quaternion()) as GameObject;
+
+            _uoots[0].transform.SetParent(_uootAniObj.transform.FindChild("Uoot_01"), false);
+            _uoots[1].transform.SetParent(_uootAniObj.transform.FindChild("Uoot_02"), false);
+            _uoots[2].transform.SetParent(_uootAniObj.transform.FindChild("Uoot_03"), false);
+            _uoots[3].transform.SetParent(_uootAniObj.transform.FindChild("Uoot_04"), false);
+
+            _uootAnimaion[(int)UootNori.Animal.DO] = Do;
+            _uootAnimaion[(int)UootNori.Animal.GE] = Ge;
+            _uootAnimaion[(int)UootNori.Animal.KUL] = Kul;
+            _uootAnimaion[(int)UootNori.Animal.UOOT] = Uoot;
+            _uootAnimaion[(int)UootNori.Animal.MO] = Mo;
+            _uootAnimaion[(int)UootNori.Animal.BACK_DO] = BackDo;
+        }
+        _uootAnimaion[(int)GameData.GetLastAnimal()]();
+        UootThrowAni();
+    }
+
+    void Do()
+    {
+        Vector3 eulerAngles;
+        int doUoot = Random.Range(0, 3);
+        for(int i = 0; i < UOOT_NUM; ++i)
+        {
+            eulerAngles = _uoots[i].transform.localEulerAngles;
+            eulerAngles.y = 0.0f;
+            _uoots[i].transform.localEulerAngles = eulerAngles;            
+        }
+
+        eulerAngles = _uoots[doUoot].transform.localEulerAngles;
+        eulerAngles.y = 180.0f;
+        _uoots[doUoot].transform.localEulerAngles = eulerAngles;
+    }
+
+    void Ge()
+    {
+        Vector3 eulerAngles;
+        int doUoot = Random.Range(0, 2);
+        int geUoot = Random.Range(2, 4);
+        for (int i = 0; i < UOOT_NUM; ++i)
+        {
+            eulerAngles = _uoots[i].transform.localEulerAngles;
+            eulerAngles.y = 180.0f;
+            _uoots[i].transform.localEulerAngles = eulerAngles;
+        }
+
+        eulerAngles = _uoots[doUoot].transform.localEulerAngles;
+        eulerAngles.y = 0.0f;
+        _uoots[doUoot].transform.localEulerAngles = eulerAngles;
+
+        eulerAngles = _uoots[geUoot].transform.localEulerAngles;
+        eulerAngles.y = 0.0f;
+        _uoots[geUoot].transform.localEulerAngles = eulerAngles;
+    }
+
+    void Kul()
+    {
+        Vector3 eulerAngles;
+        int kulUoot = Random.Range(0, 3);
+        for (int i = 0; i < UOOT_NUM; ++i)
+        {
+            eulerAngles = _uoots[i].transform.localEulerAngles;
+            eulerAngles.y = 180.0f;
+            _uoots[i].transform.localEulerAngles = eulerAngles;
+        }
+
+        eulerAngles = _uoots[kulUoot].transform.localEulerAngles;
+        eulerAngles.y = 0.0f;
+        _uoots[kulUoot].transform.localEulerAngles = eulerAngles;
+    }
+
+    void Uoot()
+    {
+        Vector3 eulerAngles;
+        for (int i = 0; i < UOOT_NUM; ++i)
+        {
+            eulerAngles = _uoots[i].transform.localEulerAngles;
+            eulerAngles.y = 180.0f;
+            _uoots[i].transform.localEulerAngles = eulerAngles;
+        }
+    }
+
+    void Mo()
+    {
+        Vector3 eulerAngles;
+        for (int i = 0; i < UOOT_NUM; ++i)
+        {
+            eulerAngles = _uoots[i].transform.localEulerAngles;
+            eulerAngles.y = 0.0f;
+            _uoots[i].transform.localEulerAngles = eulerAngles;
+        }
+    }
+
+    void BackDo()
+    {
+        Vector3 eulerAngles;
+        for (int i = 0; i < UOOT_NUM; ++i)
+        {
+            eulerAngles = _uoots[i].transform.localEulerAngles;
+            eulerAngles.y = 180.0f;
+            _uoots[i].transform.localEulerAngles = eulerAngles;
+        }
+
+        eulerAngles = _uoots[3].transform.localEulerAngles;
+        eulerAngles.y = 0.0f;
+        _uoots[3].transform.localEulerAngles = eulerAngles;
+    }
+
+    // Use this for initialization    
+    void Start () {
 	
 	}
 	
@@ -43,7 +174,7 @@ public class UootThrow : Attribute {
         if (_isDone)
             return;
 
-        if (_curTime < .2f)
+        if (_curTime < 4.0f)
         {
             _curTime += Time.deltaTime;
         }
@@ -65,7 +196,7 @@ public class UootThrow : Attribute {
                 {
                     AnimalProbabiley();
                     ThrowToData();
-                    UootThrowAni();
+                    UootAniInit();
                 }
                 else
                 {
@@ -79,10 +210,10 @@ public class UootThrow : Attribute {
 
 
     void OnEnable()
-    {
+    {   
         AnimalProbabiley();
         ThrowToData();
-        UootThrowAni();
+        UootAniInit();
     }
 
     void OnDisable()
@@ -125,11 +256,19 @@ public class UootThrow : Attribute {
     }
 
     void UootThrowAni()
-    {
+    {   
+        int aninum = Random.Range(1, 7);
+        string aniName;
+        if (_isOut)
+            aniName = "cliff0"+ aninum.ToString();
+        else
+            aniName = "n0" + aninum.ToString();
+        
+        _uootAni.Play(aniName);
     }
 
     bool UootThrowAniCheck()
-    {
+    {   
         return true;
     }
 
